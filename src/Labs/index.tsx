42ec90cc4856cf6b1cd553e3fb7cd99957dfdd9e
卢勇霏 <luyongfei@wifi-10-40-75-133.wifi.berkeley.edu>
2024-07-08 15:04:38 -0700

    add deliverable objects

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -10,6 +10,7 @@ export default function Labs() {
     return (
       <div>
         <h1>Labs</h1>
+        <h2>Author: Yongfei Lu</h2>
         <TOC />
         <Routes>
           <Route path="/" element={<Navigate to="Lab1" />} />
