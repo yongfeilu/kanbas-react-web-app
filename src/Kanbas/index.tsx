21f4e4a028b958805796d0f447f724d431353c6f
卢勇霏 <luyongfei@s-MacBook-Pro.local>
2024-08-12 14:04:11 -0500

    finish a6 for react

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -8,6 +8,8 @@ import store from "./store";
 import { Provider } from "react-redux";
 
 import { Routes, Route, Navigate } from "react-router";
+import Account from "./Account";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function Kanbas() {
 
@@ -56,15 +58,21 @@ export default function Kanbas() {
               <div className="wd-main-content-offset p-3">
                   <Routes>
                       <Route path="/" element={<Navigate to="Dashboard" />} />
-                      <Route path="Account" element={<h1>Account</h1>} />
-                      <Route path="Dashboard" element={<Dashboard
-                          courses={courses}
-                          course={course}
-                          setCourse={setCourse}
-                          addNewCourse={addNewCourse}
-                          deleteCourse={deleteCourse}
-                          updateCourse={updateCourse}/>} />
-                      <Route path="Courses/:id/*" element={<Courses courses={courses}/>} />
+                      <Route path="/Account/*" element={<Account />} />
+                      <Route path="Dashboard" element={
+                        <ProtectedRoute>
+                          <Dashboard
+                              courses={courses}
+                              course={course}
+                              setCourse={setCourse}
+                              addNewCourse={addNewCourse}
+                              deleteCourse={deleteCourse}
+                              updateCourse={updateCourse}/>
+                        </ProtectedRoute>
+                      } />
+                      <Route path="Courses/:id/*" element={
+                        <ProtectedRoute><Courses courses={courses}/></ProtectedRoute>
+                      } />
                       <Route path="Calendar" element={<h1>Calendar</h1>} />
                       <Route path="Inbox" element={<h1>Inbox</h1>} />
                   </Routes>
